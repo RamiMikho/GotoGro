12ae46943f144d964922b04f55b4dc4ee1b7f529
RamiMikho <rami.sa.mikho@gmail.com>
2022-10-23 04:59:34 +1100

    edit sales not fully implemented

--- add-sale.php
+++ add-sale.php
@@ -66,17 +66,34 @@
     echo "</fieldset>
     <input type=\"submit\" name=\"addSale\" value=\"Add\"/>
     </form>";
+    echo "
+    <form action=\"add-sale.php\" method=\"post\" style=\"width:760px\">
+    <fieldset>
+        <legend>Edit Sales</legend>
+        <select name=\"customerID\">";
+        $names = mysqli_query($conn,"SELECT * FROM customer");
+        while($customerRow = mysqli_fetch_assoc($names)){
+            $fName = $customerRow["firstName"];
+            $lName = $customerRow["lastName"];
+            $IDs = $customerRow["customerID"];
+            echo "<option value=\"$IDs\">$fName $lName</option>";
+        };
+
+    echo "</fieldset>
+    <input type=\"submit\" name=\"selectCustomerSale\" value=\"Edit Customer Sale\"/>
+    </form>";
     }
 
 
     //THIS SECTION PRETAINS TO ADDING SALES RECORDS THIS WILL BE MOVED TO
     //Initializing variables
-    $customerID = null;
+    if(isset($_POST["addSale"])){
+    $customerID = $_POST["customerID"];
     $cart = null;
     $validSaleInput = false;
     
     //Get Item
-    catchVarItem("customerID",$customerID);
+    //catchVarItem("customerID",$customerID);
     catchVarItem("cart", $cart);
 
     //Add to the Sales sql table
@@ -86,7 +103,8 @@
         $total = 0;
         //calculate the total
         foreach ($cart as $fruit){  //loop through each item checked, multiply by quantity and sum total
-            catchVarItem("quantity$fruit", $quantity);// quantity$fruit = quanity+fruitname
+            //catchVarItem("quantity$fruit", $quantity);// quantity$fruit = quanity+fruitname
+            $quantity = $_POST["quantity$fruit"];
             $price = mysqli_query($conn, "SELECT price FROM item WHERE productName=\"$fruit\";")->fetch_row()[0] ?? false;
             $total = $total + $price * $quantity;
         }
@@ -109,7 +127,8 @@
         foreach ($cart as $fruit){ 
             $itmeID = mysqli_query($conn, "SELECT itemID FROM item WHERE productName=\"$fruit\";")->fetch_row()[0] ?? false;//gets itemID from database
             $price = mysqli_query($conn, "SELECT price FROM item WHERE productName=\"$fruit\";")->fetch_row()[0] ?? false;//gets price from database
-            catchVarItem("quantity$fruit", $quantity); //catches quantity for fruit
+            //catchVarItem("quantity$fruit", $quantity); //catches quantity for fruit
+            $quantity = $_POST["quantity$fruit"];
             $query = "INSERT INTO $sqlTable (saleID, itemID, quantity, price) 
             VALUES ($saleID,$itmeID, $quantity, $price*$quantity)";
             $result = mysqli_query($conn, $query);
@@ -123,6 +142,79 @@
             }
         }
     };
+    }
+    //PRINTS THE FORM FOR EDITING
+    if(isset($_POST["selectCustomerSale"])){
+
+        $customerID = $_POST["customerID"];
+        $_SESSION["customerID"] = $_POST["customerID"];
+        //echo $_SESSION["customerID"];
+        echo "
+        <form action=\"add-sale.php\" method=\"post\" style=\"width:760px\">
+        <fieldset>
+        <select name=\"saleID\">";//Display all sales of the customer
+        $sales = mysqli_query($conn,"SELECT * FROM sale WHERE customerID = '$customerID'");
+        while($saleRow = mysqli_fetch_assoc($sales)){
+            $saleID = $saleRow["saleID"];
+            $totalPrice = $saleRow["totalPrice"];
+            $date = $saleRow["date"];
+            echo "<option value=\"$IDs\">Sale ID:$saleID Total Price:$totalPrice Date of Sale:$date</option>";
+        };
+        echo "</select>
+        <ul style=\"list-style-type: none;\">";
+        $result = mysqli_query($conn, "SELECT * FROM item");
+        //Loops through the items
+        while ($row = mysqli_fetch_assoc($result)) { 
+            echo "<li style=\"display: inline-block;\">";
+            $value = $row["productName"];
+            echo "<input type=\"checkbox\" name=\"cart[]\" value=\"$value\" class=\"fruit\"/>
+                <label for=\"$value\"><img src=\"/gotogro/images/$value.png\" width=\"100px\">$value</label>
+                <label for=\"quantity\">quantity</label>
+                <input type=\"text\" name=\"quantity$value\"/>";
+            echo "</li>";
+        };
+        echo "</ul>
+        <input type=\"submit\" name=\"editSale\" value=\"Edit a Sale\"/>
+        </form>";
+
+    }
+
+    //MODIFIES THE DATABASES VALUES
+    if(isset($_POST["editSale"])){
+    
+    $customerID = $_SESSION["customerID"];
+    $cart = null;
+    $saleID = $_POST["saleID"];
+
+    catchVarItem("cart", $cart);
+
+    $sqlTable="sale";
+    if($validSaleInput and isset($_POST["editSale"])){
+        $currentDate = date("Y-m-d");
+        $total = 0;
+        //calculate the total
+        foreach ($cart as $fruit){  //loop through each item checked, multiply by quantity and sum total
+            //catchVarItem("quantity$fruit", $quantity);// quantity$fruit = quanity+fruitname
+            $quantity = $_POST["quantity$fruit"];
+            $price = mysqli_query($conn, "SELECT price FROM item WHERE productName=\"$fruit\";")->fetch_row()[0] ?? false;
+            $total = $total + $price * $quantity;
+        }
+        //INSERT STATEMENT
+        $query = "INSERT INTO $sqlTable (customerID, totalPrice, date) VALUES ($customerID, $total, '$currentDate')";
+        $result = mysqli_query($conn, $query);
+        if(!$result){
+           echo $conn->error,"<p>Something went wrong </p>";
+        }
+        else{
+        //PUT SUCCESS NOTIFICATION/EVENT HERE
+
+        }
+    };
+
+        
+
+
+    }
     
     //CLOSE CONNECTION
     mysqli_close($conn);
@@ -141,9 +233,16 @@
     function catchVarItem($input, &$var){
         global $validSaleInput;
         if(isset($_POST[$input])){
-            $var = $_POST[$input];
-            $validSaleInput = True;
+        foreach($_POST[$input] as $item){
+            if(!isset($item)){
+                $validSaleInput = false;
+            }
+            else{
+                $var = $_POST[$input];
+                $validSaleInput = True;
+            }
         }
+    }
     }
 
     //Assings vars of search
